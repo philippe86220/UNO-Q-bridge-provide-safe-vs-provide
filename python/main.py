5fcd0d7fa50fb97fbb94f3a55a6641a919b4dd64
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-03-16 11:30:42 +0100

    Implement loop function to update Bridge with x

--- python/main.py
+++ python/main.py
@@ -1,1 +1,14 @@
+from arduino.app_utils import App, Bridge
+import time
 
+x = 0
+
+def loop():
+    global x
+    Bridge.call("update", x)
+    x += 1
+    if x > 50:
+        x = 0
+    time.sleep(0.02)
+
+App.run(user_loop=loop)
